d720f55d5491e6b3cf02127774dd0554b61cccfc
Martin Gäckler <git@gaeckler.at>
2026-09-25 16:42:23 +0200

    Add an app icon
    for use with the windows explorer

--- moonLander_rc.h
+++ moonLander_rc.h
@@ -1,6 +1,8 @@
 
-#define IDI_MOON_LANDER			1
-#define IDI_FIRE				2
-#define IDI_CRASHED_LANDER		3
+#define IDI_MOON_APP			1
+
+#define IDI_MOON_LANDER			10
+#define IDI_FIRE				11
+#define IDI_CRASHED_LANDER		12
 
 #define IDB_RISING_EARTH		40
